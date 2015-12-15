388f1bd6f8f312628e75922a06a5ea1268312c23
protoman9000 <oyekan900@yahoo.com>
2015-12-14 19:15:41 -0800

    Playing around with HTMLAgilityPack.

---
+++ webSitePuller/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace webSitePuller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> imageList = new List<string>();
+
+            //Enter the webiste that we want.
+            string webSite = Console.ReadLine();
+            WebClient targetSite = new WebClient();
+
+            string source = targetSite.DownloadString(webSite);
+
+            HtmlAgilityPack.HtmlDocument tDocument = new HtmlAgilityPack.HtmlDocument();
+            tDocument.LoadHtml(source);
+
+            foreach (var link in tDocument.DocumentNode.Descendants("img").Select(x => x.Attributes["src"]))
+            {
+                imageList.Add(link.Attributes["src"].Value.ToString());
+            }
+            
+        }
+    }
+}
